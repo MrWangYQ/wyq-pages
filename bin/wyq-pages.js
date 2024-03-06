55cc6393fd3522ae45e6a16025e3eb7a038a300b
MrWangYQ <10397638@qq.com>
2024-03-06 23:52:13 +0800

    feat: note commit

--- bin/wyq-pages.js
+++ bin/wyq-pages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-// process.argv // 数组 根据数组信息，在手动添加命令
+// process.argv // 数组 根据数组信息，在手动添加命令,替代了手工添加 
 
 process.argv.push('--cwd')
-process.argv.push(process.cwd())
-process.argv.push('--gulpfile')
+process.argv.push(process.cwd()) // 工作目录
+process.argv.push('--gulpfile') // 指定 gulpfile 地址
 process.argv.push(require.resolve('..'))
 
-require('gulp/bin/gulp');
+require('gulp/bin/gulp');// 会执行 gulp-cli 执行 gulpfile.js  读取 pages.config.js 覆盖路径
